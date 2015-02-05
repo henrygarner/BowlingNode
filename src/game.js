01c4c2886a18842a8e431169746d204de1f55356
Henry Garner <henry@henrygarner.com>
2015-02-05 13:32:08 +0000

    Moved roll into shot

--- src/game.js
+++ src/game.js
@@ -2,31 +2,30 @@ var Shot = require('./shot');
 
 function Game() {};
 
-Game._bonus = function(previousRoll, shot, roll, rolls) {
-    if (shot.isStrike(roll) && shot.frame < 9) {
+Game._bonus = function(previousShot, roll, rolls) {
+    if (Shot.isStrike(roll) && previousShot.frame < 9) {
         return rolls[1] + rolls[2]; // Strike
-    } else if (previousRoll + roll === 10 && shot.ball === 1) {
+    } else if (previousShot.roll + roll === 10 && previousShot.ball === 1) {
         return rolls[1]; // Spare
     }
     return 0;
 };
 
-Game._scorer = function(previousRoll, shot, score, rolls) {
+Game._scorer = function(score, previousShot, rolls) {
     var roll = rolls[0];
     if (roll === undefined) {
         return score;
     } else {
-        var bonus = this._bonus(previousRoll, shot, roll, rolls);
-        return this._scorer(roll,
-                            shot.next(roll),
-                            score + roll + bonus,
+        var bonus = this._bonus(previousShot, roll, rolls);
+        return this._scorer(score + roll + bonus,
+                            previousShot.next(roll),
                             rolls.slice(1));
     }
 };
 
 Game.score = function() {
-    var init = new Shot(0, 0);
-    return this._scorer(0, init, 0, [].slice.call(arguments));
+    var init = new Shot(0, 0, 0);
+    return this._scorer(0, init, [].slice.call(arguments));
 };
 
 module.exports = Game;
--- src/shot.js
+++ src/shot.js
@@ -1,25 +1,22 @@
-function Shot(frame, ball) {
+function Shot(frame, ball, roll) {
     this.frame = frame;
     this.ball = ball;
+    this.roll = roll;
 };
 
-Shot.prototype.isSpare = function(roll) {
-    return this.ball === 0 && roll != 10;
-};
-
-Shot.prototype.isStrike = function(roll) {
+Shot.isStrike = function(roll) {
     return roll === 10;
 };
 
 Shot.prototype.next = function(roll) {
-    if (this.isSpare(roll) ||
+    if (this.ball === 0 && !Shot.isStrike(roll) ||
         this.frame === 10 && roll === 10) {
         if (this.ball > 1) {
             throw 'Too many rolls';
         }
-        return new Shot(this.frame, this.ball + 1);
+        return new Shot(this.frame, this.ball + 1, roll);
     } else {
-        return new Shot(this.frame + 1, 0);
+        return new Shot(this.frame + 1, 0, roll);
     }
 };
 
